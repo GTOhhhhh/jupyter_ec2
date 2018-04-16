d49803b90be16afa27eae8628c7c921e959ee7de
Karl Spiegel <spiegel.karl@gmail.com>
2018-04-16 13:59:39 +1000

    work directory now set by argument (default is current directory)

--- jupyter.sh
+++ jupyter.sh
@@ -2,13 +2,14 @@
 
 #!/bin/bash
 echo "This script will install Docker for Ubuntu EC2 then launch
- an encrypted Juptyer Notebook inside the docker container"
+an encrypted Juptyer Notebook inside the docker container"
+echo "This script will launch a containerized encrypted Jupyter Notebook"
 sudo apt-get -y install language-pack-en
 sudo apt-get -y update
 sudo apt-get -y full-upgrade
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
-sudo apt-key add
+   sudo apt-key add
 echo "Checking docker installer gpg key..."
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update
@@ -17,4 +18,6 @@ sudo usermod -aG docker $USER
 echo "Building docker notebook image"
 container=`sudo docker build . | tail -c 13`
 echo "### container_id = $container ### "
-sudo docker run -it --rm -p 8888:8888 -v /usr/proj/ripe:/home/jovyan/work -e GEN_CERT=yes $container start-notebook.sh
+value=${1:-`pwd`}
+sudo docker run -it --rm -p 8888:8888 -v $value:/home/jovyan/work -e GEN_CERT=yes $container start-notebook.sh --NotebookApp.password='sha1:ba031f6f03f8:bfbb198af8b70438adda40a42197c69bb9d9eb30'
+
